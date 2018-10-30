23140fc4877c3ad515bf996a433634fefc2e7c5e
Szymon Filipiak <szymonfilipiak98@gmail.com>
2018-10-30 13:58:41 +0000

    Added downloader for coinz locations. Connected to MapBox using access token.

--- app/src/main/java/com/example/szymon/coinz/MainActivity.kt
+++ app/src/main/java/com/example/szymon/coinz/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.szymon.coinz
 
+
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+
+
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+    private var mapView: MapView? = null
+    private var map: MapboxMap? = null
+
+    //private lateinit var originLocation : Location
+    //private lateinit var permissionsManager : PermissionsManager
+    //private lateinit var locationEngine : LocationEngine
+    //private lateinit var locationLayerPlugin : LocationLayerPlugin
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +40,15 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        Mapbox.getInstance(applicationContext, getString(R.string.access_token))
+        mapView = findViewById(R.id.mapboxMapView)
+        mapView?.onCreate(savedInstanceState)
+        //mapView?.getMapAsync(this)
+
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,5 +64,56 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+
     }
+
+    interface DownloadCompleteListener {
+        fun downloadComplete(result: String)
+    }
+
+    object DownloadCompleteRunner : DownloadCompleteListener {
+        var result : String? = null
+        override fun downloadComplete(result: String) {
+            this.result = result
+        }
+    }
+
+    class DownloadFileTask(private val caller : DownloadCompleteListener):
+            AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg urls: String): String = try {
+            loadFileFromNetwork(urls[0])
+        } catch (e: IOException) {
+            "Unable to load content, Check your network connection"
+        }
+
+        private fun loadFileFromNetwork(urlString: String): String {
+            val stream : InputStream = downloadUrl(urlString)
+            // Read input from stream, build result as a string
+            val result = stream.bufferedReader().use { it.readText() }
+            return result
+        }
+
+        @Throws(IOException::class)
+        private fun downloadUrl(urlString: String): InputStream {
+            val url = URL(urlString)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.readTimeout = 10000
+            conn.connectTimeout = 15000
+            conn.requestMethod = "GET"
+            conn.doInput = true
+            conn.connect()
+            return conn.inputStream
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+            caller.downloadComplete(result)
+        }
+
+    } //end class DownloadFileTask
+
+
+
+
 }
